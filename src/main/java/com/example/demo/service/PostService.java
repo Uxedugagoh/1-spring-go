3ab3396b9d423a977a14dffd6e9cc70466ea9a29
homemade <agent-001panamka@mail.ru>
2025-10-16 22:38:33 +0500

    Функционал добавления постов добавлен

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -9,13 +9,23 @@ import java.util.List;
 
 @Service
 public class PostService {
+    private final List<Post> posts;
+
+    public PostService(List<Post> posts) {
+        this.posts = posts;
+        this.posts.add(new Post("Убийца плачет", new Date()));
+        this.posts.add(new Post("Но он не смог ... поступить иначе", new Date()));
+        this.posts.add(new Post("Прольётся ... кроооооооовь", new Date()));
+        this.posts.forEach(p -> p.setLikes(0));
+    }
 
     public List<Post> listAllPosts() {
-        List<Post> finalList = new ArrayList<>();
-        finalList.add(new Post("Убийца плачет", new Date()));
-        finalList.add(new Post("Но он не смог ... поступить иначе", new Date()));
-        finalList.add(new Post("Прольётся ... кроооооооовь", new Date()));
-        finalList.forEach(p -> p.setLikes(0));
-        return finalList;
+        return posts;
+    }
+
+    public void create(String text) {
+        Post post = new Post(text, new Date());
+        post.setLikes(0);
+        posts.add(post);
     }
 }
